a764319f95b39d64245041df3cf6a4fe5c38a8b5
Eduardo Bautista <eduardobautistauribe7@gmail.com>
2023-12-30 11:37:36 -0600

    finished

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ app.use(express.static("public"));
 app.get("/", async (req, res) => {
     try {
         const result = await axios.post("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json");
-        // const responseObject = JSON.parse(jsonString);
-        console.log(result);
-        // console.log(JSON.parse(result.data));
         res.render("index.ejs", {quote: result.data.quoteText, author: result.data.quoteAuthor})
     } catch (error){
         console.log(error.response.data);
